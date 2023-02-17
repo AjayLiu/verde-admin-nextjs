a836d525c0905fb45d6a0d81079c19180a64e6e1
AjayLiu <ajayliu2928@gmail.com>
2023-02-16 17:13:36 -0800

    Initialize deploy

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from "next/app";
+
+import "@styles/globals.scss";
+import GoogleAnalyticsHook from "@components/GoogleAnalyticsHook";
+function Application({ Component, pageProps }: AppProps) {
+  return <>
+    <GoogleAnalyticsHook />
+    <Component {...pageProps} />
+  </>
+}
+
+export default Application;
