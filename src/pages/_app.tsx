ad6228b92e8e6dcd721a2ca1164f8ed412b1fc48
AjayLiu <ajayliu2928@gmail.com>
2023-02-21 20:07:19 -0800

    Everything works ig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import type { AppProps } from "next/app";
 
 import "@styles/globals.scss";
 import GoogleAnalyticsHook from "@components/GoogleAnalyticsHook";
+import "react-datetime-picker/dist/DateTimePicker.css";
+import "react-calendar/dist/Calendar.css";
+// import "react-clock/dist/Clock.css";
+
 function Application({ Component, pageProps }: AppProps) {
   return (
     <>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import Head from "next/head";
 import Footer from "@components/Footer/Footer";
 import { useEffect, useState } from "react";
-import { Timestamp, setDoc, doc } from "firebase/firestore";
+import {
+  Timestamp,
+  setDoc,
+  doc,
+  getDocs,
+  collection,
+} from "firebase/firestore";
 import { uuidv4 } from "@firebase/util";
 import { database } from "src/config/firebase";
+import DateTimePicker from "react-datetime-picker/dist/entry.nostyle";
 
 interface Challenge {
   uid: string;
@@ -15,14 +22,10 @@ interface Challenge {
 }
 
 const Home = () => {
-  const [challengeData, setChallengeData] = useState<Challenge>({
-    uid: "test",
-    title: "test",
-    description: "test",
-    points: 10,
-    startTime: Timestamp.now(),
-    expirationTime: Timestamp.now(),
-  });
+  const [challengeData, setChallengeData] = useState<Challenge>();
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   const saveChallenge = async () => {
     try {
@@ -36,6 +39,24 @@ const Home = () => {
     }
   };
 
+  const [existingChallenges, setExistingChallenges] = useState<Challenge[]>();
+  // Fetch current challenges
+  useEffect(() => {
+    const fetchChallenges = async () => {
+      try {
+        const challenges = await getDocs(collection(database, "challenges"));
+        const challengeList = challenges.docs.map((c) =>
+          c.data()
+        ) as Challenge[];
+        console.log(challengeList);
+        setExistingChallenges(challengeList);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchChallenges();
+  }, []);
+
   return (
     <>
       <Head>
@@ -43,15 +64,91 @@ const Home = () => {
       </Head>
 
       <main>
+        <h1>Verde Admin Page</h1>
+        <div style={{ marginBottom: "100px" }}>
+          <h2>Create a new Challenge:</h2>
+          <div>
+            <label>Challenge Title</label>
+            <input
+              placeholder="Challenge Title"
+              onChange={(e) =>
+                setChallengeData({ ...challengeData, title: e.target.value })
+              }
+              value={challengeData?.title}
+            />
+          </div>
+          <div>
+            <label>Challenge Description</label>
+            <input
+              placeholder="Challenge Description"
+              onChange={(e) =>
+                setChallengeData({
+                  ...challengeData,
+                  description: e.target.value,
+                })
+              }
+              value={challengeData?.description}
+            />
+          </div>
+          <div>
+            <label>Challenge Points</label>
+            <input
+              placeholder="Challenge Points"
+              onChange={(e) =>
+                setChallengeData({
+                  ...challengeData,
+                  points: parseInt(e.target.value),
+                })
+              }
+              value={challengeData?.points}
+              datatype="number"
+            />
+          </div>
+          <div>
+            <label>Start time</label>
+            <DateTimePicker
+              value={startDate}
+              onChange={() => {
+                setStartDate(startDate);
+                setChallengeData({
+                  ...challengeData,
+                  startTime: Timestamp.fromDate(startDate),
+                });
+              }}
+            />
+          </div>
+          <div>
+            <label>End time</label>
+            <DateTimePicker
+              value={endDate}
+              onChange={() => {
+                setStartDate(endDate);
+                setChallengeData({
+                  ...challengeData,
+                  expirationTime: Timestamp.fromDate(endDate),
+                });
+              }}
+            />
+          </div>
+          <button onClick={() => saveChallenge()}>Create challenge</button>
+        </div>
         <div>
-          <input
-            placeholder="Enter the Title.."
-            onChange={(e) =>
-              setChallengeData({ ...challengeData, title: e.target.value })
-            }
-            value={challengeData?.title}
-          />
-          <button onClick={() => saveChallenge()}>Save challenge</button>
+          <h2>Existing Challenges: </h2>
+          <div>
+            {existingChallenges &&
+              existingChallenges.map((c: Challenge) => {
+                return (
+                  <div>
+                    <p>
+                      uid: {c.uid}, title: {c.title}, description:{" "}
+                      {c.description}, points: {c.points}, start:
+                      {c.startTime.toDate().toLocaleString()}, end:
+                      {c.expirationTime.toDate().toLocaleDateString()}
+                    </p>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </main>
 
